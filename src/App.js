c3966d3ce8c5d91d3dae01dd230ddf04d5135fa8
taylorsieling <taylorasieling@gmail.com>
2021-06-22 11:18:09 -0500

    setting up file structure and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1><i>Taylor Swift Rankings</i></h1>
+        </header>
+      </div>
+      <Switch>
+        <Route exact path='/' component={home}/>
+        <Route exact path='/login' component={login}/>
+        <Route exact path='/signup' component={signup}/>
+        <Route exact path='/scoring' component={scoring}/>
+        <Route exact path='/stats' component={stats}/>
+      </Switch>
+    </Router>
   );
 }
 
