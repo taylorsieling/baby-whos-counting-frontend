d38e1edb2ef94d0e69f59bdff8fa53a8abbab612
taylorsieling <taylorasieling@gmail.com>
2021-06-22 12:52:11 -0500

    builind album component for scoring

---
+++ src/components/Album.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Album = () => {
+    return (
+        <div>
+            <h2>Album Component</h2>
+        </div>
+    )
+}
+
+export default Album;
--- src/containers/Scoring.js
+++ src/containers/Scoring.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Album from '../components/Album'
 
 export default class Scoring extends Component {
     render() {
         return (
             <div>
                 <h1>Scoring</h1>
+                <Album/>
             </div>
         )
     }
