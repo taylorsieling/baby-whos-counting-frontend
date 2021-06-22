3f58110601da97126188087635246d49130cf0de
taylorsieling <taylorasieling@gmail.com>
2021-06-22 18:14:46 -0500

    building login form comp

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,12 +4,16 @@ export const LoginForm = () => {
     return (
         <div className="form-parent">
             <div className="login-form">
+                <h2><i>login with email</i></h2>
                 <form onSubmit="{props.handleSubmit}">
                     <input type="text" id="email" className="text-input" placeholder="email"></input><br/>
                     <input type="password" id="password" className="text-input" placeholder="password"></input>
                 </form>
             </div>
             <hr/>
+            <div className="form-parent">
+                <h2><i>login with google | login with spotify</i></h2>
+            </div>
         </div>
     )
 }
