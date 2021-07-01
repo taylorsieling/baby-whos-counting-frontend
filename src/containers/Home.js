e5c602f746c6de99f47535c5cf2174e752b3eead
taylorsieling <taylorasieling@gmail.com>
2021-07-01 12:26:35 -0500

    changed Home.js to functional comp

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import NavPublic from '../components/NavPublic'
 import Login from '../containers/Login'
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div className="App">
-                {/* <NavPublic/> */}
-                <header className="App-header">
-                    <h1><span className="subtitle">welcome to</span><br/>Swiftify</h1>
-                    <Login/>
-                </header>
-            </div>
-        )
-    }
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1><span className="subtitle">welcome to</span><br/>Swiftify</h1>
+                <Login/>
+            </header>
+        </div>
+    )
 }
+
+export default Home;
+
