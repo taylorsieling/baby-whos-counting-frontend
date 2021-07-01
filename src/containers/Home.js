46e21f05ae51af136f1c85bea09df33673ad665f
taylorsieling <taylorasieling@gmail.com>
2021-07-01 12:22:22 -0500

    Re-styling Home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import NavPublic from '../components/NavPublic'
+import Login from '../containers/Login'
 
 export default class Home extends Component {
     render() {
         return (
             <div className="App">
-                <NavPublic/>
+                {/* <NavPublic/> */}
                 <header className="App-header">
-                    <h3><i>welcome to</i></h3>
-                    <h1><i>Taylor Swift Rankings</i></h1>
+                    <h1><span className="subtitle">welcome to</span><br/>Swiftify</h1>
+                    <Login/>
                 </header>
             </div>
         )
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -5,13 +5,7 @@ import NavPublic from '../components/NavPublic'
 function Login() {
     return (
         <div>
-            <NavPublic/>
-            <LoginForm/>
-            <div className="form-parent">
-                <h2>
-                    <a href="http://localhost:3001/api/v1/login" className="btn btn--login">login with spotify</a>
-                </h2>
-            </div>
+            <a href="http://localhost:3001/api/v1/login" className="btn btn-login">login with spotify</a>
         </div>
     )
 }
